dd3d1db8a1bd25fc27d7bdd6f68e351df0f3a7c4
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-08-22 16:01:51 +0300

    read from dB and ReactTracker
    installed react-tracker from ultimatejs
    made simple read from dB
    {res[0].text is returning null. the info is not updated and therefore cannot be added.

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 ## list of tutorials
 https://forums.meteor.com/t/list-of-meteor-react-tutorials-courses-guides/24894
 
+# Tools to use:
+## Browser - meteortoys:allthings
+1. add this into packages: `meteortoys:allthings` and use it with `CTRL-M`
+
 # init 0
 initial meteor & react zeroBase level setup
 clone this and use it to make new projects
-
-## Tools to use:
-1. add this into packages: `meteortoys:allthings` and use it with `CTRL-M`
-
-## INIT-DO:
+## first steps:
 - `git clone git@github.com:nmsabc/l21meteor.git l21MeteorReact/` //get the initial repository
 - `cd l21MeteorReact/`
 - `npm init`    // init the npm and creake the package.json
@@ -32,3 +32,7 @@ clone this and use it to make new projects
 1. define a re var : `var Resolutions = new Mongo.Collection("resolution"); `
   - it comes with `Collections.allow` functions: `insert, update and remove`
   - we **must** add the same on `main.js` server side - else we have an error message: `insert failed: Method '/resolutions/insert' not found`
+  - also **ensure** you add `.bind(this)` in the input form `<form className = 'new-resolition' onSubmit={this.addResolution.bind(this)}>`
+    - this will permit you to export the default class App that extends the React.Component
+
+## TrackerReact - we will add a tracker react for making react components reactive :)
--- client/App.js
+++ client/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Mongo } from 'meteor/mongo';
 
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
 //we added the resolution Mongo db in here
 export const Resolutions = new Mongo.Collection("resolutions");
 Resolutions.allow({
@@ -17,36 +19,48 @@ Resolutions.allow({
 });
 
 // class App extends React.Component{
-var App = React.createClass({
+// var App = React.createClass({
+export default class App extends TrackerReact(React.Component){
 
-  addResolution(event){
-    event.preventDefault();
-    var text = this.refs.resolution.value.trim();
+      resolutions(){
+        return Resolutions.find({}).fetch();
+      }//resolutions function
 
-    Resolutions.insert({
-      text: text,
-      complete: false,
-      createdAt: new Date()
-    }); // Resolutions.insert function
+      addResolution(event){
+        event.preventDefault();
+        var text = this.refs.resolution.value.trim();
 
-    this.refs.resolution.value = "";
-  },
+        Resolutions.insert({
+          text: text,
+          complete: false,
+          createdAt: new Date()
+        }); // Resolutions.insert function
 
-  render(){
-    return(
-        <div>
-          <h1> These are my resolutions: </h1>
-          <form className = 'new-resolition' onSubmit={this.addResolution}>
-            <input type="text" ref ="resolution" placeholder="input your resolution ..."/>
-          </form>
-        </div>
-    )//return
-  }// render
-}) // class App
+        this.refs.resolution.value = "";
+      }
 
+      render(){
+        var res = this.resolutions();
+        console.info(res.length);
+        if (res.lenth< 1) {
+          return (<div> Loading ... </div>)
+        }
+  //      console.log(res[0].text);
+            return(
+                <div>
+                  <h1> These are my resolutions: </h1>
+                  <form className = 'new-resolition' onSubmit={this.addResolution.bind(this)}>
+                    <input type="text" ref ="resolution" placeholder="input your resolution ..."/>
+                  </form>
+                  </div>
+            )//return
+      }// render
+} // class App
+
+// {res[0].text}
 // we need to export this class for the router.js,
 // where we do import App and not as import {App} from App.js
-export default App;
+// export default App;
 
 // if(Meteor.isClient){
 //   Meteor.startup(function(){
