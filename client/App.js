178b7051a594da89bdba11db5dd7dc8c3b095477
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-08-22 12:58:19 +0300

    working db for Resolutions
    we have a good db working with inserts.
    see readme for how things are done

--- client/App.js
+++ client/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Mongo } from 'meteor/mongo';
+
+//we added the resolution Mongo db in here
+export const Resolutions = new Mongo.Collection("resolutions");
+Resolutions.allow({
+  insert: function(){
+    return true;
+  },
+  update: function(){
+    return true;
+  },
+  remove: function(){
+    return true;
+  }
+});
 
 // class App extends React.Component{
 var App = React.createClass({
+
+  addResolution(event){
+    event.preventDefault();
+    var text = this.refs.resolution.value.trim();
+
+    Resolutions.insert({
+      text: text,
+      complete: false,
+      createdAt: new Date()
+    }); // Resolutions.insert function
+
+    this.refs.resolution.value = "";
+  },
+
   render(){
     return(
         <div>
-          <h1> hi there </h1>
+          <h1> These are my resolutions: </h1>
+          <form className = 'new-resolition' onSubmit={this.addResolution}>
+            <input type="text" ref ="resolution" placeholder="input your resolution ..."/>
+          </form>
         </div>
     )//return
   }// render
--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+
+//  import { Mongo } from 'meteor/mongo';
+
   // code to run on server at startup
+  
+  //we need this on the server side also
+  // else we have insert failed: Method '/resolutions/insert' not found
+
+  //we added the resolution Mongo db in here
+  export const Resolutions = new Mongo.Collection("resolutions");
 });
